17e71aa2569c8833a9b26420066168bbc67da215
Noman <28227944+Nomi@users.noreply.github.com>
2026-05-07 00:49:40 +0200

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    const char open = '(';
+    const char close = ')';
+    const char wildcard = '*'; // can be '(', ')', or ' '.
+    
+    public bool CheckValidString(string s) {
+        //Wildcard can be empty string, so WRONG: // if(s.Length % 2 != 0)   return false;
+
+
+        int minOpen = 0; //MIN POSSIBLE UNBALANCED OPENS (PREFER WILDCARDS TO BE CLOSED, BUT CAN BE REBALANCED BY TREATING SOME AS EMPTY OR '(' BY RESETTING BACK TO 0 AS LONG AS THERE ARE ENOUGH WILDCARDS (proven by maxOpen > 0))
+        int maxOpen = 0; //MAX POSSIBLE UNBALANCED OPENS (when all wildcards are `open`)
+        foreach(var c in s) {
+            if(c == open) {
+                minOpen++;
+                maxOpen++;
+            }
+            else if(c == close) {
+                minOpen--;
+                maxOpen--;
+            }
+            else { //wildcard (or could check and throw for unsupported)
+                minOpen--;
+                maxOpen++;
+            }
+
+            if(maxOpen < 0)
+                return false;
+            
+            // Rebalancing: we treat earlier `*` as ` ` OR `)`
+            //
+            // If there were not enough (or at all) wildcards before this to rebalance, MAX open would be < 0 and
+            // hence the above condition would handle it (wait but what if maxOpen == 0??? We can't rebalance thhen??) 
+            if(minOpen < 0) 
+                minOpen = 0; //I didn't even think of this, got it from the neetcode solution I peeked at earlier :'( 
+        }
+
+        return (minOpen == 0);
+    }
+}
