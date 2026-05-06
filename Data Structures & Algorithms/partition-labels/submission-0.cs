3db94f44784b7b47ee2ce576557de58dae817093
Noman <28227944+Nomi@users.noreply.github.com>
2026-05-07 00:17:14 +0200

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<int> PartitionLabels(string s) {
+        List<int> partLengths = [];
+
+        Dictionary<char, int> lastIdx = new();
+
+        for(int i = 0; i < s.Length; i++) {
+            lastIdx[s[i]] = i;
+        }
+
+        for(int l = 0; l < s.Length; ) {
+
+            var curPartLast = lastIdx[s[l]]; //idx
+
+            for(int r = l; r <= curPartLast; r++) { 
+                curPartLast = Math.Max(curPartLast, lastIdx[s[r]]);
+            }
+
+            partLengths.Add(curPartLast + 1 - l); //*IMPORTANT* Since this is length, we must calculate it BEFORE updating l!
+            l = curPartLast + 1; //next part's first idx
+        }
+
+        return partLengths;
+    }
+}
