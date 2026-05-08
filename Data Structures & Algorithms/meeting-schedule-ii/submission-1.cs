5103bf7956044bc4e88cb2d66f6dfcdd832cfc43
Noman <28227944+Nomi@users.noreply.github.com>
2026-05-08 22:21:58 +0200

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cs
@@ -0,0 +1,43 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    // Took me 10minutes 41seconds to write the code
+    // TC = O(2n * log(2n)) = O(n*log(n))
+    // SC = O(2n) = O(n)
+    public int MinMeetingRooms(List<Interval> intervals) {
+        //min number of rooms == maximum overlap
+
+        List<(int ts, bool isEnd)> events = new();
+
+        foreach(var interval in intervals){
+            events.Add((interval.start, false));
+            events.Add((interval.end, true));
+        }
+        
+        // for sorting, false == 0, isEnd == 1
+        events = events.OrderBy(e => e.ts).ThenByDescending(e => e.isEnd).ToList(); //OrderBy Ascending, and ENDS FIRST (because of our requirement `(0,8),(8,10) is NOT considered a conflict at 8.`)
+
+        int maxOverlap = 0;
+        int curCount = 0;
+        foreach(var e in events){
+            if(e.isEnd) {
+                curCount --;
+            }
+            else {
+                curCount++;
+                if(maxOverlap < curCount) maxOverlap = curCount;
+            }
+        }
+
+        return maxOverlap;
+    }
+}
