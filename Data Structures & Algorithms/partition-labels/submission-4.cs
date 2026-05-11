70fd6465ceac21a4c78c1a841f238e616d72a250
Noman <28227944+Nomi@users.noreply.github.com>
2026-05-11 13:24:55 +0200

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    const int LowerCaseEnglishLetterCount = 26;
+    public List<int> PartitionLabels(string s) { // Kind of like `intervals` :)
+        List<int> partLengths = [];
+
+        Dictionary<char, int> lastIdx = new(); //Last Appearance Idx (value) of each char (key)
+
+        for(int i = 0; i < s.Length; i++) {
+            lastIdx[s[i]] = i;
+        }
+        
+        //Micro Optimization:        // for(int i = s.Length-1; i >= 0; i--) {        //     lastIdx.TryAdd(s[i], i);        //     if(lastIdx.Count == LowerCaseEnglishLetterCount) break; //Only works if all chars in string!        // }
+
+        for(int l = 0; l < s.Length; ) {
+
+            var curPartLast = lastIdx[s[l]]; //idx
+
+            for(int r = l; r <= curPartLast; r++) { 
+                curPartLast = Math.Max(curPartLast, lastIdx[s[r]]);
+            }
+
+            partLengths.Add(curPartLast + 1 - l); //*IMPORTANT* Since this is length, we must calculate it BEFORE updating l!
+            l = curPartLast + 1; //next part's first idx
+        }
+
+        return partLengths;
+    }
+}
